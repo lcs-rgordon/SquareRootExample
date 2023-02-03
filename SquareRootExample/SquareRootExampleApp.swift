d7aa1321eee83c95c09c523400cc389d131ba3a2
Russell Gordon <rgordon@lcs.on.ca>
2023-02-03 13:39:17 -0500

    Initial Commit

---
+++ SquareRootExample/SquareRootExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareRootExampleApp.swift
+//  SquareRootExample
+//
+//  Created by Russell Gordon on 2023-02-03.
+//
+
+import SwiftUI
+
+@main
+struct SquareRootExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
